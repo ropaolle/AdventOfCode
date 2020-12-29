a90a907525d5c4a83343f30db46712c0d8a95453
ropaolle <ropaolle@gmail.com>
2020-12-29 16:28:19 +0100

    day 9

---
+++ 2020/day09/index.js
@@ -0,0 +1,59 @@
+const { loadData, numSort } = require('../../lib.js');
+
+const rawData = loadData('data.txt').map((v) => Number(v));
+
+const sumExist = (val, arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === val) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+const partOne = (data) => {
+  const preambleSize = 25;
+  let window = data.slice(0, preambleSize);
+
+  for (let i = preambleSize; i < data.length - preambleSize; i++) {
+    const val = data[i];
+    const exists = sumExist(val, window);
+    if (!exists) {
+      console.log(exists, i, val, window, val, data[i - 1]);
+      return val;
+    }
+    window.shift();
+    window.push(val);
+  }
+};
+
+const partTwo = (data) => {
+  const test = 177777905; //127;
+
+  for (let i = 0; i < data.length; i++) {
+    let j = i;
+    let sum = 0;
+    let list = [];
+    let min = test;
+    let max = 0;
+
+    while (sum < test) {
+      j += 1;
+      const val = data[j];
+      list.push(val);
+      min = Math.min(min, val);
+      max = Math.max(max, val);
+      sum += val;
+    }
+    if (sum === test) {
+      // console.log('sum, min, max', sum, min, max);
+      return min + max;
+    }
+  }
+};
+
+console.clear();
+console.table({ partOne: partOne(rawData), partTwo: partTwo(rawData) });
+// process.exit(2)
