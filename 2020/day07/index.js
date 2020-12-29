14505b6df3d7cae840b1b65868514db617da6259
ropaolle <ropaolle@gmail.com>
2020-12-29 11:10:01 +0100

    day 7

--- 2020/day07/index.js
+++ 2020/day07/index.js
@@ -34,76 +34,41 @@ const partOne = (data) => {
   return set.size;
 };
 
-const getContent = (bagType, data) =>
+const getContent = (bagType, data, multiplicator) =>
   bagsThatCanContainOtherBags(data)
     .filter(({ type }) => type === bagType)
-    // .map(({ data }) => data.split(', '));
     .reduce((acc, { data }) => {
       return acc.concat(data.split(', '));
     }, [])
     .map((v) => {
       const splitted = v.split(' ');
       return {
-        count: splitted[0],
+        count: Number(splitted[0]) * multiplicator,
         type: splitted[1] + ' ' + splitted[2],
       };
     });
 
 const partTwo = (data) => {
-  let x = getContent('shiny gold', data);
-  console.log('x', x);
+  let bags = getContent('shiny gold', data, 1);
+  let i = 0;
+  let sum = 0;
+  let prevSum = 0;
 
-  const y = x.reduce((acc, v) => {
-    console.log('v', v.type);
-    const t = getContent(v.type, data);
-    console.log('t', t);
-    acc = acc.concat(t);
-    return acc;
-  }, []);
+  do  {
+    prevSum = sum;
+    bags = bags.reduce((acc, v) => {
+      const { count } = v;
+      sum += count;
+      return [...acc, ...getContent(v.type, data, count)];
+    }, []);
+    
+    // console.log('Loops', i, sum);
+    i += 1;
+  } while (sum > prevSum)
 
-  console.log('y', y);
-
-  // while (x.length > 0) {
-
-  // }
-
-  // shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
-  // faded blue bags contain 0 other bags.
-  // dotted black bags contain 0 other bags.
-  // vibrant plum bags contain 11 other bags: 5 faded blue bags and 6 dotted black bags.
-  // dark olive bags contain 7 other bags: 3 faded blue bags and 4 dotted black bags.
-  // const a1 = bagsThatCanContainOtherBags
-  //   .filter(({ type }) => type === 'shiny gold')
-  //   .map(({ data }) => data.split(', '));
-  // console.log('bagsThatCanContainOtherBags', a1);
-
-  // const x = getContent('shiny gold', data).map(v => {
-  //   console.log('v', v);
-  //   const splitted = v.split(' ')
-  //   return {
-  //     count: splitted[0],
-  //     type: splitted[1] + ' ' + splitted[2],
-
-  //   }
-  // })
-  // console.log(x);
-  // console.log(getContent('pale green', data));
-  // console.log(getContent('faded gold', data));
-
-  /*
-  '5 striped white bags',
-    '1 mirrored coral bag',
-    '3 dark plum bags',    
-  //   '5 clear teal bags'  */
-  // console.log(getContent('triped white', data));
-  // console.log(getContent('mirrored coral', data));
-  // console.log(getContent('dark plum', data));
-  // console.log(getContent('clear teal',data));
-
-  return 'OLLE';
+  return sum;
 };
 
-console.clear();
-// console.log(partTwo(data))
+// console.clear();
 console.table({ partOne: partOne(data), partTwo: partTwo(data) });
-//  process.exit(2)
+// process.exit(2)
