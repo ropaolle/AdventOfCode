b0d4b3bcb2f7ee6ab46a68da6e65ec937128be3b
Olle <olle.sjogren@teleoffice.se>
2020-12-31 16:45:35 +0100

    day 11

--- 2020/day11/index.js
+++ 2020/day11/index.js
@@ -1,60 +1,94 @@
 const { loadData } = require('../../lib.js');
+const data = loadData(__dirname, 'data.txt').map((v) => v.split(''));
 
-const data = loadData(__dirname, 'data-test.txt').join('');
+const width = data[0].length;
+const height = data.length;
 
-const pattern0 =
-  'L.LL.LL.LLLLLLLLL.LLL.L.L..L..LLLL.LL.LLL.LL.LL.LLL.LLLLL.LL..L.L.....LLLLLLLLLLL.LLLLLL.LL.LLLLL.LL';
-const pattern1 =
-  '#.LL.L#.###LLLLLL.L#L.L.L..L..#LLL.LL.L##.LL.LL.LL#.LLLL#.##..L.L.....#LLLLLLLL##.LLLLLL.L#.#LLLL.##';
-const pattern5 =
-  '#.#L.L#.###LLL#LL.L#L.#.L..#..#L##.##.L##.#L.LL.LL#.#L#L#.##..L.L.....#L#L##L#L##.LLLLLL.L#.#L#L#.##';
+const adjacent = [
+  [-1, -1],
+  [0, -1],
+  [1, -1],
+  [-1, 0],
+  [1, 0],
+  [-1, 1],
+  [0, 1],
+  [1, 1],
+];
 
-const width = 10;
-const adjacent = [-width + 1, -width + 2, -width + 3 - 1, 1, width - 3, width - 2, width - 1];
+const getSeat = (row, col, generation) => {
+  return typeof generation[row] !== 'undefined' && typeof generation[row][col] !== 'undefined'
+    ? generation[row][col]
+    : null;
+};
 
-// TODO: Stor data in array
+const occupiedNeighbours = (row, col, levels, threshold, generation) => {
+  const neighbours = [null, null, null, null, null, null, null, null];
+  let level = 1;
 
-const nextGeneration = (gen) => {
-  let next = '';
-  [...gen].forEach((v, i) => {
-    //   console.log('i % width', i, i % width);
-    if (
-      v === 'L' &&
-      adjacent.every((v) => gen[i + v] === undefined || gen[i + v] === 'L' || gen[i + v] === '.')
-    ) {
-      next += '#';
-    } else if (v === '#' && adjacent.filter((v) => gen[i + v] === '#').length > 3) {
-      next += 'L';
-    } else {
-      next += v;
+  while (neighbours.filter((v) => v === '#').length < threshold && level <= levels) {
+    for (let i = 0; i < adjacent.length; i++) {
+      if (neighbours[i] !== '#' && neighbours[i] !== 'L') {
+        neighbours[i] = getSeat(
+          row + adjacent[i][1] * level,
+          col + adjacent[i][0] * level,
+          generation
+        );
+      }
     }
-  });
+    level += 1;
+  }
+
+  return neighbours.filter((v) => v === '#').length;
+};
+
+const nextGeneration = (generation, levels, threshold) => {
+  // Deep cloning
+  let next = JSON.parse(JSON.stringify(generation));
+
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const occupied = occupiedNeighbours(row, col, levels, threshold, generation);
+      const val = generation[row][col];
+
+      if (val === 'L' && occupied === 0) {
+        next[row][col] = '#';
+      } else if (val === '#' && occupied >= threshold) {
+        next[row][col] = 'L';
+      } else {
+        next[row][col] = val;
+      }
+    }
+  }
+
+  if (generation.flat().join('') === next.flat().join('')) {
+    return next.flat().filter((x) => x === '#').length;
+  }
+
   return next;
 };
 
 const partOne = () => {
   let next = data;
-  console.log(next);
-  next = nextGeneration(next);
-  console.log(next);
-  next = nextGeneration(next);
-  console.log(next);
-  next = nextGeneration(next);
-  console.log(next);
-  next = nextGeneration(next);
-  console.log(next);
-  next = nextGeneration(next);
-  console.log(next);
-  next = nextGeneration(next);
-  console.log(pattern5);
+  do {
+    next = nextGeneration(next, 1, 4);
+  } while (Array.isArray(next));
 
+  return next;
 };
 
-const partTwo = () => {};
+const partTwo = () => {
+  let next = data;
+  do {
+    next = nextGeneration(next, Math.max(width, height), 5);
+  } while (Array.isArray(next));
 
-console.clear();
+  return next;
+};
+
+// console.clear();
+// partOne()
 console.log('Part one:', partOne());
-// console.log('Part two:', partTwo());
+console.log('Part two:', partTwo());
 
 // Exports
 exports.partOne = partOne;
