146af68f75ee43cd195685bc728b6892ec3462eb
Olle <olle.sjogren@teleoffice.se>
2021-01-01 22:41:03 +0100

    day 13

---
+++ 2020/day14/index.js
@@ -0,0 +1,15 @@
+const { loadData } = require('../../lib.js');
+
+const data = loadData(__dirname, 'data-test.txt', { numeric: false, sorted: false });
+
+const partOne = () => {};
+
+const partTwo = () => {};
+
+// console.clear();
+// console.log('Part one:', partOne());
+// console.log('Part two:', partTwo());
+
+// Exports
+exports.partOne = partOne;
+exports.partTwo = partTwo;
